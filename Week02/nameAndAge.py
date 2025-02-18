67e4c3ddce18cb78ac4745b73a2b39149fa8db07
Mr-Scarf <dscal07@outlook.com>
2025-02-18 07:06:36 +0000

    'week02'

---
+++ Week02/nameAndAge.py
@@ -0,0 +1,8 @@
+# nameAndAge.py - Week2
+# This program reads a name & age & outputs a greeting & states the age
+# Author: David Scally
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+
+print(f'Hello {name}, your age is {age}.')
